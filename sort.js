a525cd8160c75a4465980ac211cc4944a6af740e
Haque-Misbahul <mhaque.tuc@gmail.com>
2024-08-02 00:23:28 +0200

    Added pagination code

--- sort.js
+++ sort.js
@@ -13,3 +13,16 @@ db.inventory.find().skip(5)
 db.inventory.find().sort({qty: 1})
 //sort descending order
 db.inventory.find().sort({qty: -1})
+
+//Paginatio using MondoDB find and limit example
+//1 to 5
+ no = 10
+
+ db.blogs.find().skip((pageNo-1)*no).limit(no)
+
+ pageno=1 //db.blogs.find().skip(0).limit(10)
+ pageno=2 //db.blogs.find().skip(10).limit(10)
+ pageno=3 //db.blogs.find().skip(20).limit(10)
+ pageno=4 //db.blogs.find().skip(30).limit(10)
+ pageno=5 //db.blogs.find().skip(40).limit(10)
+
